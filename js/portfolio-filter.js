1b4b07224ef98f4eb1b71a0c4fbfaf79065f42c9
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-12-30 14:15:43 +0200

    Updated the base themes, frameworks, plugins

--- js/portfolio-filter.js
+++ js/portfolio-filter.js
@@ -1,12 +1,26 @@
-$(function() {
-    var selectedClass = "";
-    $(".fil-cat").click(function() {
-        selectedClass = $(this).attr("data-rel");
-        $("#portfolio-gallery").fadeTo(100, 0.1);
-        $("#portfolio-gallery .portfolio-item").not("." + selectedClass).fadeOut().removeClass('scale-anm');
-        setTimeout(function() {
-            $("." + selectedClass).fadeIn().addClass('scale-anm');
-            $("#portfolio-gallery").fadeTo(300, 1);
-        }, 300);
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
     });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
 });
